1a7c0b3f644e892216bc6691ccf87277fd25cff7
Evan Wappel <ewappel@gmail.com>
2017-11-01 16:59:03 -0500

    test

---
+++ js_test.js
@@ -0,0 +1,8 @@
+var gdal = require("gdal");
+var dataset = gdal.open("category_optical_splitter.shp");
+var layer = dataset.layers.get(0);
+
+console.log("number of features: " + layer.features.count());
+console.log("fields: " + layer.fields.getNames());
+console.log("extent: " + JSON.stringify(layer.extent));
+console.log("srs: " + (layer.srs ? layer.srs.toWKT() : 'null'));
